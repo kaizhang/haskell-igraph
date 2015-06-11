a3958109de3af59b7addaa0545315519d98b3fd5
Kai Zhang <kai@kzhang.org>
2015-06-11 10:33:00 -0700

    change function signature for cliques

--- src/IGraph/Clique.hs
+++ src/IGraph/Clique.hs
@@ -10,20 +10,20 @@ import IGraph
 import IGraph.Internal.Clique
 import IGraph.Internal.Data
 
-cliques :: (Int, Int)  -- ^ Minimum and maximum size of the cliques to be returned.
+cliques :: LGraph d v e
+        -> (Int, Int)  -- ^ Minimum and maximum size of the cliques to be returned.
                        -- No bound will be used if negative or zero
-        -> LGraph d v e
         -> [[Int]]     -- ^ cliques represented by node ids
-cliques (lo, hi) gr = unsafePerformIO $ do
+cliques gr (lo, hi) = unsafePerformIO $ do
     vpptr <- igraphVectorPtrNew 0
     _ <- igraphCliques (_graph gr) vpptr lo hi
     (map.map) truncate <$> vectorPPtrToList vpptr
 
-maximalCliques :: (Int, Int)  -- ^ Minimum and maximum size of the cliques to be returned.
+maximalCliques :: LGraph d v e
+               -> (Int, Int)  -- ^ Minimum and maximum size of the cliques to be returned.
                               -- No bound will be used if negative or zero
-               -> LGraph d v e
                -> [[Int]]     -- ^ cliques represented by node ids
-maximalCliques (lo, hi) gr = unsafePerformIO $ do
+maximalCliques gr (lo, hi) = unsafePerformIO $ do
     vpptr <- igraphVectorPtrNew 0
     _ <- igraphMaximalCliques (_graph gr) vpptr lo hi
     (map.map) truncate <$> vectorPPtrToList vpptr
